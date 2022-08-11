ddd0d8c31451efd4ce43b7e8de204c4d83c58176
WIN-2ULUTV73FC1\Admin <sergeii.kirik@gmail.com>
2022-08-12 00:45:20 +0300

    deploy fix

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
 app.use(express.json({extended: true}));
-app.use('/', require('./routes/routes'));
 
 app.use(express.static(path.join(__dirname, 'frontend')));
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
@@ -20,6 +19,8 @@ app.get('/', function (req, res) {
   console.log('get')
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
+
+app.use('/', require('./routes/routes'));
 
 async function start() {
   try {
